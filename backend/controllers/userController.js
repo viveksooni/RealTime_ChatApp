82cfe6a3bee59f5724e9d01a44d7dbc05d0ba11a
viveksooni <69984313+viveksooni@users.noreply.github.com>
2024-02-10 09:31:59 +0000

    userController Update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,22 @@
-module.exports.register = (req,res,next)=>{
-    console.log(req.body);
-}
+const { mongo, default: mongoose } = require("mongoose");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+module.exports.register = async (req, res, next) => {
+  const { userName, email, password } = req.body;
+
+  const userNameCheck = await User.findOne({ userName });
+
+  if (userNameCheck) {
+    return res.json({ msg: "userName already exists" });
+  }
+  const emailCheck = await User.findOne({ email });
+
+  if (emailCheck) {
+    return res.json({ msg: "email already exists" });
+  }
+
+  const hashedPassword = bcrypt.hash(password, 10);
+
+  const user = await User.create({ email, userName, password: hashedPassword });
+  delete user.password;
+};
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,2 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const userSchema = mongoose.Schema({
+  userName: { type: String, required: true, min: 3, max: 20, unique: true },
+  password: { type: String, required: true, min: 8, unique: true },
+  email: { type: String, required: true, max: 50 },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: false,
+  },
+  AvatarImage: { type: String, default: "" },
+});
+
+module.exports = mongoose.model("Users",userSchema)
